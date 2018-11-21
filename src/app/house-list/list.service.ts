4bd63a3c8cf99348b8ac41ebc9455d9ec97059da
Avhijit Dutta <counter.jit@gmail.com>
2018-11-21 08:39:28 +0530

    Update url

--- src/app/house-list/list.service.ts
+++ src/app/house-list/list.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class ListService {
-  private heroesUrl = 'https://anapioficeandfire.com/api/houses';  // URL to web api
+  private heroesUrl = environment.baseUrl + 'houses';  // URL to web api
 
   constructor(private httpClient: HttpClient) {
 
